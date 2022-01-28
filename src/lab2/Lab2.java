b446c69ab5062261fe73404466d292002b059247
DELL <DELL@BavneetKaur>
2022-01-27 17:05:31 -0700

    Created a new Java Application for lab 2

--- src/lab2/Lab2.java
+++ src/lab2/Lab2.java
@@ -1,13 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package lab2;
 
 /**
  *
- * @author DELL
+ * @author Bavneet
  */
 public class Lab2 {
 
@@ -15,7 +10,12 @@ public class Lab2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int a = 7;
+        int b = 1;
+        
+        int c = a + b;
+        
+        System.out.println(c);
     }
     
 }
